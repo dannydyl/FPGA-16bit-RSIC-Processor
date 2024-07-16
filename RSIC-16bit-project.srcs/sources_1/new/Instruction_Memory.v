4b805f04b27c49c8c3a54e60198fc9f78a908535
Dongyun Lee <dannydyl8@gmail.com>
2024-07-16 00:43:05 -0400

    added test folder and test.prog

--- RSIC-16bit-project.srcs/sources_1/new/Instruction_Memory.v
+++ RSIC-16bit-project.srcs/sources_1/new/Instruction_Memory.v
@@ -19,7 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+// instructions are in the memory and the pc[4:1] is going to point at the address where the instruction is stored
 module Instruction_Memory(
         input[15:0] pc,     // program counter
         output[15:0] instruction
@@ -30,7 +30,7 @@ module Instruction_Memory(
     wire [3:0] rom_addr = pc[4:1]; // same as delcaring signal rom_addr and do rom_addr <= pc(4 downto 1);
     
     initial begin
-        $readmemb("file path", memory, 0, 14);  // verilog's %readmemb is a system task to load binary data into an array
+        $readmemb("C:\Users\danny\OneDrive\Desktop\FPGA\RSIC-16bit-project\test\test.prog", memory, 0, 14);  // verilog's %readmemb is a system task to load binary data into an array
     end
     assign instruction = memory[rom_addr]; // equivalent to instruction <= memory(to_integer(unsigned(rom_addr))); 
 endmodule
